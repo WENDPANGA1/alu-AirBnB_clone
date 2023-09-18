3e9172e790e2f81dc379996b98ddf6ea67840dc3
WENDPANGA1 <w.ima@alustudent.com>
2023-09-18 15:55:00 +0000

    done

--- models/city.py
+++ models/city.py
@@ -6,5 +6,9 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """ City class """
+
     state_id = ""
     name = ""
+
+    def __init__(self, *args, **kwargs):
+         super().__init__(*args, **kwargs)
