7f207cf3e2662dff620b0986412480b2aadc5b47
WENDPANGA1 <w.ima@alustudent.com>
2023-09-18 15:38:09 +0000

    done

--- models/base_model.py
+++ models/base_model.py
@@ -1,63 +1,72 @@
 #!/usr/bin/python3
-"""Defines BaseModel class."""
-import models
-from uuid import uuid4
+"""
+Parent class to all classes in the AirBnB clone project
+"""
+
+
 from datetime import datetime
+from uuid import uuid4
+import models
 
 
 class BaseModel:
-    """
-        Class Base
-        Defines all common attributes/methods for other classes
-        Attr :
-                id: string - assigned with an uuid when an instance is created
-                created_at: datetime - assigned with the current datetime
-                when an instance is created
+    """Custom BaseModel class for the AirBnB clone project
+    Attributes:
+        id (str): unique id for each instance
+        created_at (datetime): time object was created
+        updated_at (datetime): time object was updated
 
-                updated_at: datetime - assigned with the current datetime
-                when an instance is created.
-                It will be updated every time the object change.
+    Methods:
+        __init__: initializes class instance
+        __str__: returns string representation of class instance
+        save: updates attribute updated_at with current datetime
+        to_dict: returns dictionary representation of class instance
     """
 
     def __init__(self, *args, **kwargs):
-        """Initialize new BaseModel."""
-
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-        if kwargs:
-            kwargs["created_at"] = datetime.strptime(
-                kwargs["created_at"], tform)
-            kwargs["updated_at"] = datetime.strptime(
-                kwargs["updated_at"], tform)
-            del kwargs["__class__"]
-            self.__dict__.update(kwargs)
-        else:
+        """initializes class instance
+        Args:
+            *args: unused
+            **kwargs: dictionary of attributes
+        """
+        DATE_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
+        if not kwargs:
             self.id = str(uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             models.storage.new(self)
+        else:
+            for key, v in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    self.__dict__[key] = datetime.strptime(v, DATE_TIME_FORMAT)
+                elif key[0] == "id":
+                    self.__dict__[key] = str(v)
+                else:
+                    self.__dict__[key] = v
+
+    def __str__(self):
+        """returns string representation of class instance
+        Returns:
+            str: string representation of class instance
+        """
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Set updated_at with current datetime."""
-        self.updated_at = datetime.now()
+        """updates attribute updated_at with current datetime"""
+        self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """Return dictionary of BaseModel instance.
-
-        Includes key/value pair __class__.
+        """returns dictionary representation of class instance
+        Returns:
+            dict: dictionary representation of class instance
         """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
-
-    def __str__(self):
-        """Return print/str representation of BaseModel instance."""
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+        new_objects = {}
+        for key, value in self.__dict__.items():
+            if key in ["created_at", "updated_at"]:
+                new_objects[key] = value.isoformat()
+            else:
+                new_objects[key] = value
+        new_objects["__class__"] = self.__class__.__name__
+        return new_objects
